ce6016623dd6365439ae3705b1370ee8b0356b39
Ashley Yaner <ashley.yaner@fractureid.com>
2023-05-04 07:59:33 -0600

    Day 33: interactive weather forecast app. Pulls data from openweathermap.org. User inputs location and if the location doesn't exists, shows message to user. Slider bar to select number of days to forecast. Pulldown menu with options to show sky and tempurature. If sky, then show condition image and date below image. If temperature, show graph of temp vs day.

---
+++ backend.py
@@ -0,0 +1,20 @@
+import requests
+
+# Go to openweathermap.prg and request an API key and paste it in API_KEY for this to work
+API_KEY = ""
+
+
+def get_data(place, forecast_days):
+    url = "https://api.openweathermap.org/data/2.5/" \
+          f"forecast?q={place}&" \
+          f"appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data['list']
+    num_values = 8 * forecast_days
+    filtered_data = filtered_data[:num_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=3))
